10796d629fac98f84c8c3723598148f491caa22b
gabrielnsd <gabrielnsdantas@gmail.com>
2024-06-27 18:51:31 -0300

    WIP: implemented node ffi

---
+++ nodejs/index.js
@@ -0,0 +1,90 @@
+// @ts-check
+"use strict";
+
+/**
+ * @module metal-translator
+ */
+
+const { load, DataType, open, close, define } = require("ffi-rs");
+const path = require("path");
+
+const dynamicLib = "./libmetaltranslate.so";
+// TODO: set dynamicLib according to OS
+
+open({
+  library: "metalTranslate",
+  path: dynamicLib,
+});
+
+const metal = define({
+  create_metal_translate: {
+    library: "metalTranslate",
+    retType: DataType.External,
+    paramsType: [DataType.String],
+  },
+  translate: {
+    library: "metalTranslate",
+    retType: DataType.String,
+    paramsType: [
+      DataType.External,
+      DataType.String,
+      DataType.String,
+      DataType.String,
+    ],
+  },
+  free_metal_translate: {
+    library: "metalTranslate",
+    retType: DataType.Void,
+    paramsType: [DataType.External],
+  },
+});
+
+class Translator {
+  /**
+   *
+   * @param {string} modelPath
+   */
+  constructor(modelPath) {
+    this.translator = metal.create_metal_translate([
+      modelPath + modelPath.endsWith("/") ? "" : "/",
+    ]);
+  }
+
+  /**
+   *
+   * @param {string} text
+   * @param {string} sourceCode
+   * @param {string} targetCode
+   */
+  translate(text, sourceCode, targetCode) {
+    const result = metal.translate([
+      this.translator,
+      text,
+      sourceCode,
+      targetCode,
+    ]);
+    console.log("result in method", result);
+    return result;
+  }
+
+  free() {
+    metal.free_metal_translate([this.translator]);
+  }
+}
+
+// Example usage:
+/** 
+const modelPath = path.resolve("..", "models/translate-fairseq_m2m_100_418M/");
+
+const testTranslator = new Translator(modelPath + "/");
+
+const textToTranslate = "This is a test of translation";
+
+const result = testTranslator.translate(textToTranslate, "en", "es");
+
+testTranslator.free();
+
+console.log("final result: ", result);
+*/
+
+exports.Translator = Translator;
--- src/MetalTranslateConfig.h
+++ src/MetalTranslateConfig.h
@@ -3,6 +3,11 @@
 namespace MetalTranslate {
 class MetalTranslateConfig {
 public:
+  MetalTranslateConfig() = default;
+
+  MetalTranslateConfig(const std::string& model_path) : ModelPath(model_path) {}
+
+
   std::string ModelPath = "models/translate-fairseq_m2m_100_418M/";
 };
 } // namespace MetalTranslate
